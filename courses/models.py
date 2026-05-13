f08dd37e1deb97a554469f646f3d934800c5baf5
Mike Devereux <Michael.Devereux@unibas.ch>
2026-05-13 09:13:04 +0200

    Created Course model

--- courses/models.py
+++ courses/models.py
@@ -25,3 +25,32 @@ class Module(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Course(models.Model):
+    code = models.CharField(max_length=32, unique=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    credit_points = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        verbose_name='credit points',
+    )
+    category = models.ForeignKey(
+        CourseCategory,
+        on_delete=models.PROTECT,
+        related_name='courses',
+    )
+    modules = models.ManyToManyField(
+        Module,
+        related_name='courses',
+        blank=True,
+    )
+    active = models.BooleanField(default=True)
+    notes = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ['code']
+
+    def __str__(self) -> str:
+        return f'{self.code} - {self.title}'
