fde8e4056335065424dd8e4eaa6a966571763b01
Mike Devereux <Michael.Devereux@unibas.ch>
2026-05-13 09:06:34 +0200

    Created CourseCategory model

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class CourseCategory(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    display_order = models.PositiveIntegerField(default=0)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['display_order', 'name']
+        verbose_name = 'course category'
+        verbose_name_plural = 'course categories'
+
+    def __str__(self) -> str:
+        return self.name
